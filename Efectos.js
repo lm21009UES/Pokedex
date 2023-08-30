be847d76ddb08afce8328a905fb80388de5d4ada
JohnArbaiza1 <johnarbaiza07.03@gmail.com>
2023-08-30 15:12:12 -0600

    Funcionalidades + pequeños aspectos del diseño

--- Efectos.js
+++ Efectos.js
@@ -13,4 +13,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
   
+//Aca se pograma la funcionalidad del button de regresar al navbar en el index
+//Definimos una funcion para regresar al navbar la cual tiene un parametro que es pxPantalla  
+function subir(pxPantalla){
+
+  //Evento que se encargara del dezplazamiento
+  window.addEventListener('scroll' , () =>{
+
+    //Obtenemos la posicion vertical del desplazamiento y la asignamos a scroll
+     let  scroll = document.documentElement.scrollTop;
+     // console.log(scroll);
+
+     //const del button
+     const buttonSubir = document.getElementById('Regresar');
+
+     //verificamos si la posicion del desplazamiento es mayor que el de pxPantalla
+     if(scroll > pxPantalla){
+      buttonSubir.style.right = '20px';
+     } 
+     else{
+      //ocultamos el button en caso de que no sea mayor
+      buttonSubir.style.right = '-100px';
+     }
+  }) 
+}
+//Llamamos a la funcion y le otorgamos un valor
+subir(330);
   
